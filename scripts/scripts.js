d00a91ce75cf5b9d3a824c007fd7b3fe0b6aafec
klaus <oriokiklaus@gmail.com>
2019-02-18 09:06:19 +0300

    enter a condition to prompt the user to enter an actual value when the input is 0

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,9 @@ if
 ((side1+side2)<=side3|| (side2+side3)<=side1|| (side1+side3)<=side2){
 alert("That is not a triangle!");
 }
+else if (side1===0 && side2===0 && side3=== 0){
+  alert("please insert a value");
+}
 else if (side1 === side2 && side1 ===  side3 && side2 === side3){
 alert("this is an equilateral triangle");
 }
